c31c5ece171495a8d9dc4aad62beda3f04c75835
Luuk W. (Twinone) <twinonedevs@gmail.com>
2019-04-08 19:13:21 +0200

    Add debugging options

---
+++ src/config.js
@@ -0,0 +1,30 @@
+const EventEmitter = require('events')
+const util = require("./util")
+
+
+class Config extends EventEmitter {
+    constructor() {
+        super()
+
+
+        this.loadUrl = util.getQueryParam('loadUrl') || ""
+
+        // print settings
+        this.currentLayerNumber = 0
+        this.layerHeight = 0.2
+        this.nozzleSize = 0.4
+
+        // debugging options
+        this.axesHelper = true
+        this.wireframe = false
+
+        // transformations
+        this.rotation = {x: 0, y: 0, z: 0}
+        this.scale = {x: 1, y: 1, z: 1}
+
+        // misc
+        this.epsilon = 1e-10 // in mm
+    }
+}
+
+module.exports = Config
